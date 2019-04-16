f10b7b7c8812b5508a7ba9f0077fff9cf68a63c9
Alan Scherger <alan@netifi.com>
2019-04-16 15:06:08 -0400

    ci: add publish task appropriate builds

--- ci/travis.sh
+++ ci/travis.sh
@@ -13,11 +13,11 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_TAG" == "" ]; then
     # Develop Branch
     echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-    ./gradlew clean generatePomFileForMavenBomPublication artifactoryPublish --stacktrace --refresh-dependencies
+    ./gradlew clean generatePomFileForMavenBomPublication publish artifactoryPublish --stacktrace --refresh-dependencies
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" == release/* ]] && [ "$TRAVIS_TAG" == "" ]; then
     # Release Branch
     echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']'
-    ./gradlew clean generatePomFileForMavenBomPublication artifactoryPublish --stacktrace --refresh-dependencies
+    ./gradlew clean generatePomFileForMavenBomPublication publish artifactoryPublish --stacktrace --refresh-dependencies
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
     # Master Branch
     echo -e 'Build Master for Release => Branch ['$TRAVIS_BRANCH']'
@@ -25,9 +25,9 @@ elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; 
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
     # Tag
     echo -e 'Build Tag for Release => Tag ['$TRAVIS_TAG']'
-    ./gradlew clean generatePomFileForMavenBomPublication bintrayUpload --stacktrace --refresh-dependencies
+    ./gradlew clean generatePomFileForMavenBomPublication publish artifactoryPublish bintrayUpload --stacktrace --refresh-dependencies
 else
     # Feature Branch
     echo -e 'Build Branch => Branch ['$TRAVIS_BRANCH']'
-    ./gradlew clean generatePomFileForMavenBomPublication artifactoryPublish --stacktrace --refresh-dependencies
+    ./gradlew clean generatePomFileForMavenBomPublication publish artifactoryPublish --stacktrace --refresh-dependencies
 fi
